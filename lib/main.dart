81bd32c29102447da3d1963d6f4c71a3a1c9f152
Robrownn <robrownn@gmail.com>
2018-03-15 15:25:51 -0400

    Initial Boilerplate

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
@@ -8,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Flutter Demo',
-      theme: new ThemeData(
+      theme: new ThemeData.dark(
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
@@ -17,93 +19,96 @@ class MyApp extends StatelessWidget {
         // "hot reload" (press "r" in the console where you ran "flutter run",
         // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
         // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+        // primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
+class AccountData {
+  String name = '';
+  Set<Character> characters = new Set<Character>();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class Character {
+  String name = '';
+  String ring = '';
+  String ring2 = '';
+  String weapon = '';
+  String weapon2 = '';
+  String bodyArmor = '';
+  String amulet = '';
+  String belt = '';
+  String gloves = '';
+  String offhand = '';
+  String offhand2 = '';
+  Set<String> flasks = new Set<String>();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({ Key key }) : super(key: key);
+  @override
+  createState() => new HomePageState();
+}
+  
+class HomePageState extends State<HomePage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  AccountData account = new AccountData();
+
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+
+  bool _autovalidate = false;
+  bool _formWasEdited = false;
+
+  void _handleSubmitted() {
+    final FormState form = _formKey.currentState;
+    if (!form.validate()) {
+      _autovalidate = true;
+    } else {
+      form.save();
+    }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+  String _validateName(String value) {
+    _formWasEdited = true;
+    if (value.isEmpty) {
+      return 'Name is required';
+    }
+
+    return null;
+
+    Future<bool> _warnUserAboutInvalidData() async {
+      final FormState form =_formKey.currentState;
+      if (form == null || !_formWasEdited || form.validate())
+        return true;
+
+      return await showDialog<bool>(
+        context: context,
+        builder: (BuildContext context) {
+          return new AlertDialog(
+            title: const Text('This form has errors'),
+            content: const Text('This form has invalid data, are you sure you want to leave?'),
+            actions: <Widget>[
+              new FlatButton(
+                child: const Text('YES'),
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                }
+              ),
+            ],
+          );
+        },
+      ) ?? false;
+    }
+  }
+
+  Widget _buildAccountName() {
+    return new TextFormField(
+      
     );
   }
 }
+
